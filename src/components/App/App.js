7cc513d7de79de95bfac609fcfba720c74353e3b
NateLog <nmd.wright@gmail.com>
2024-08-04 16:07:03 +0000

    from codespace

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+//import Game from "../Game";
+//import Header from "../Header";
+
+function App() {
+  return (
+    <div>
+   
+      <div >
+        Holy shit how did that work???
+      </div>
+    </div>
+  );
+}
+
+export default App;
